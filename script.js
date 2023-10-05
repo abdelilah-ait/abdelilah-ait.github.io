d23a55a29d7f86b31056e95e64fa57a4cc26edd1
abdelilah-ait <a.aitlahcen@yahoo.fr>
2023-10-05 13:48:17 +0200

    debug 3

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var originalLink = document.querySelector('a[href="http://bitofdeal.com/cl/180_md/2/14/0/0/0"]');
+    var originalLink = document.querySelector('a[href="[placeholder1]#[url]"]');
     
     if (originalLink) {
         alert("Script executed!");
-        var redirectLink = 'https://www.anrdoezrs.net/click-100951081-15494765';
+        var redirectLink = '[placeholder5]';
 
         originalLink.addEventListener('click', function (event) {
             event.preventDefault();
