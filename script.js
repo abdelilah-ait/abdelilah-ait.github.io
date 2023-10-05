b154713a688ee13984e0026c7b004bdd994d2b97
abdelilah-ait <a.aitlahcen@yahoo.fr>
2023-10-05 20:21:02 +0200

    debug 9

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // var originalLink = document.querySelector('a[href="[placeholder1]#[url]"]');
-    
-    // if (originalLink) {
-    //     alert("Script executed!");
-    //     var redirectLink = '[placeholder5]';
-
-    //     originalLink.addEventListener('click', function (event) {
-    //         event.preventDefault();
-    //         window.location.href = redirectLink;
-    //     });
-    // }
-
-
-    // var redirectLink = 'https://www.anrdoezrs.net/click-100951081-15494765';
-    // window.location.href = redirectLink;
-
     var redirectLink = '[placeholder5]';
     var imageLink = document.getElementById('redirectLink');
 
@@ -25,7 +9,3 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 });
-
-// window.onload = function () {
-//     // Aucun code supplémentaire à exécuter ici
-// };
