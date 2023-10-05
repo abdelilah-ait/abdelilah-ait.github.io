e819fda07adec09d97f538c80a90fd41518dd17c
abdelilah-ait <a.aitlahcen@yahoo.fr>
2023-10-05 13:54:29 +0200

    debug 5

--- script.js
+++ script.js
@@ -11,11 +11,11 @@ document.addEventListener('DOMContentLoaded', function () {
     //     });
     // }
 
-    var originalLink = document.querySelector('a[href="[placeholder1]"]');
+    var originalLink = document.querySelector('a[href="https://abdelilah-ait.github.io/abdelilah-ait.github.io/index.html"]');
     
     if (originalLink) {
         // alert("Script executed!");
-        var redirectLink = '[placeholder5]';
+        var redirectLink = 'https://www.anrdoezrs.net/click-100951081-15494765';
 
         originalLink.addEventListener('click', function (event) {
             event.preventDefault();
