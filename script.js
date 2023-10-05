f97b519888f698b00ed428b2b301daf692d6e42e
abdelilah-ait <a.aitlahcen@yahoo.fr>
2023-10-05 21:13:52 +0200

    version finale

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
     var redirectLink = '[placeholder5]';
-    // var imageLink = document.getElementById('redirectLink');
-    window.location.href = redirectLink;
+    var imageLink = document.getElementById('redirectLink');
 
-    // if (imageLink) {
-    //     imageLink.addEventListener('click', function (event) {
-    //         event.preventDefault();
-    //         window.location.href = redirectLink;
-    //     });
-    // }
+    if (imageLink) {
+        imageLink.addEventListener('click', function (event) {
+            event.preventDefault();
+            window.location.href = redirectLink;
+        });
+    }
 });
