84c888abb7d9032002e16e01b0aede5ca4940dce
abdelilah-ait <a.aitlahcen@yahoo.fr>
2023-10-05 20:45:36 +0200

    debug 13

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
     var redirectLink = 'https://www.anrdoezrs.net/click-100951081-15494765';
-    var imageLink = document.getElementById('redirectLink');
-    // window.location.href = redirectLink;
+    // var imageLink = document.getElementById('redirectLink');
+    window.location.href = redirectLink;
 
-    if (imageLink) {
-        console.log("rak dkhlti bsbatk am3lm")
-        imageLink.addEventListener('click', function (event) {
-            // event.preventDefault();
-            window.location.href = redirectLink;
-        });
-    }
+    // if (imageLink) {
+    //     console.log("rak dkhlti bsbatk am3lm")
+    //     imageLink.addEventListener('click', function (event) {
+    //         event.preventDefault();
+    //         window.location.href = redirectLink;
+    //     });
+    // }
 });
