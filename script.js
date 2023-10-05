4aaf09a6c968d181ac04caaf13c05dcb5131333b
abdelilah-ait <a.aitlahcen@yahoo.fr>
2023-10-05 20:43:52 +0200

    debug 12

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
     var redirectLink = 'https://www.anrdoezrs.net/click-100951081-15494765';
     var imageLink = document.getElementById('redirectLink');
+    // window.location.href = redirectLink;
 
     if (imageLink) {
         console.log("rak dkhlti bsbatk am3lm")
         imageLink.addEventListener('click', function (event) {
-            event.preventDefault();
+            // event.preventDefault();
             window.location.href = redirectLink;
         });
     }
